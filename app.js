372b41d8e7c90913fe6a77c0d8562a498a843cf7
Natasha <112275600+natashachinea@users.noreply.github.com>
2023-01-16 10:29:10 +0000

    app,server and json added

---
+++ app.js
@@ -0,0 +1,18 @@
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+
+const app = express();
+const PORT = 3001;
+
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+
+app.use("/api/create", createRouter);
+app.use("/api/join", joinRouter);
+app.use("/api/view", viewRouter);
+
+app.listen(PORT, function () {
+  console.log(`server is running in port ${PORT}`);
+});
