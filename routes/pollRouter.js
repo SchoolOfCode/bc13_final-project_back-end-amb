c1d91443f7ca787b00907af1d9ef66eb00352b24
Natasha <112275600+natashachinea@users.noreply.github.com>
2023-01-17 11:08:50 +0000

    poll router added co-authored-by: Atyib aatyib@gmail.com co-authored-by: Kylie k_smaldon@outlook.com

--- routes/pollRouter.js
+++ routes/pollRouter.js
@@ -2,17 +2,11 @@ import express from "express";
 
 const pollRouter = express.Router();
 
+import { createPoll } from "../models/poll.js";
 
+pollRouter.post("/", async function (req, res) {
+  const result = await createPoll(req.body);
+  res.json({ success: true, payload: result });
+});
 
-
-
-
-
-
-
-
-
-
-
-
-export default pollRouter
+export default pollRouter;
