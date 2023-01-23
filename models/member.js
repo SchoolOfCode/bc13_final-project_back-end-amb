c5147db20a2b1fc4f112097a0970e9c670a6969c
Natasha <112275600+natashachinea@users.noreply.github.com>
2023-01-23 16:49:53 +0000

    memberemails added

--- models/member.js
+++ models/member.js
@@ -1,9 +1,15 @@
 import query from "../database/index.js";
 
 export async function createMember(members) {
+  console.log(members);
   const newTrip = await query(
     "INSERT INTO members (trip_id, user_id) VALUES ($1, $2) RETURNING *",
-    [members.trip_id, members.user_id]
+    [members.trip_id.trip_id, members.data.user_id]
   );
-  return newTrip.rows;
+  //console.log(newTrip);
+  const memberEmail = await query(
+    "INSERT INTO members_emails (trip_id, user_name, user_email) VALUES ($1, $2, $3) RETURNING *",
+    [members.trip_id.trip_id, members.data.user_name, members.data.user_email]
+  );
+  return { newTrip: newTrip.rows, memberEmail: memberEmail.rows };
 }
--- routes/memberRouter.js
+++ routes/memberRouter.js
@@ -2,25 +2,12 @@ import express from "express";
 
 const memberRouter = express.Router();
 
-
 import { createMember } from "../models/member.js";
 
-
 memberRouter.post("/", async function (req, res) {
+  console.log(req.body);
   const result = await createMember(req.body);
   res.json({ success: true, payload: result });
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-export default memberRouter
+export default memberRouter;
