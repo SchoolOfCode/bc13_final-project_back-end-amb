3834cad80f55988f111178f4ecc228c11ddcbd79
Natasha <112275600+natashachinea@users.noreply.github.com>
2023-01-24 11:38:33 +0000

    nodemailer added

--- models/member.js
+++ models/member.js
@@ -1,4 +1,5 @@
 import query from "../database/index.js";
+import nodemailer from "nodemailer";
 
 export async function createMember(members) {
   console.log(members);
@@ -7,19 +8,41 @@ export async function createMember(members) {
     [members.trip_id, members.user_id]
   );
   //console.log(newTrip);
- 
-  return { newTrip: newTrip.rows};
+
+  return { newTrip: newTrip.rows };
 }
 
-
 export async function emailMember(data) {
-  console.log(data)
+  console.log(data);
   const memberEmail = await query(
     "INSERT INTO members_emails (trip_id, user_name, user_email) VALUES ($1, $2, $3) RETURNING *",
     [data.trip_id, data.user_name, data.user_email]
   );
 
-
-
-  //, memberEmail: memberEmail.rows 
+  for (let i = 0; i < trip.member.length; i++) {
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: "travelherd2023@gmail.com",
+        pass: "wmygxapigknbfktk",
+      },
+    });
+    const mailOptions = {
+      from: "travelherd2023@gmail.com",
+      to: `${data.user_email}`,
+      subject: "You've been invited to a new trip",
+      text: `A friend has invited you to join a group trip on travel herd! to join, navigate to travelherd.com, log in and go to join trip. 
+        then enter '${data.trip_id}'`,
+    };
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log(`email sent: ${info.response}`);
+      }
+    });
+    //, memberEmail: memberEmail.rows
+    //password: wmygxapigknbfktk
+  }
+  return {};
 }
