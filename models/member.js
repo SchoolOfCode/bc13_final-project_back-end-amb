86f3be4c8cc4b8ef0817b33c369843748c01e31d
L-E-W-1-5 <lewiswootton88@hotmail.com>
2023-01-24 10:43:12 +0000

    email fix

--- models/member.js
+++ models/member.js
@@ -4,12 +4,22 @@ export async function createMember(members) {
   console.log(members);
   const newTrip = await query(
     "INSERT INTO members (trip_id, user_id) VALUES ($1, $2) RETURNING *",
-    [members.trip_id.trip_id, members.data.user_id]
+    [members.trip_id, members.user_id]
   );
   //console.log(newTrip);
+ 
+  return { newTrip: newTrip.rows};
+}
+
+
+export async function emailMember(data) {
+  console.log(data)
   const memberEmail = await query(
     "INSERT INTO members_emails (trip_id, user_name, user_email) VALUES ($1, $2, $3) RETURNING *",
-    [members.trip_id.trip_id, members.data.user_name, members.data.user_email]
+    [data.trip_id, data.user_name, data.user_email]
   );
-  return { newTrip: newTrip.rows, memberEmail: memberEmail.rows };
+
+
+
+  //, memberEmail: memberEmail.rows 
 }
--- routes/memberRouter.js
+++ routes/memberRouter.js
@@ -2,11 +2,17 @@ import express from "express";
 
 const memberRouter = express.Router();
 
-import { createMember } from "../models/member.js";
+import { createMember, emailMember } from "../models/member.js";
 
 memberRouter.post("/", async function (req, res) {
   console.log(req.body);
   const result = await createMember(req.body);
+  res.json({ success: true, payload: result });
+});
+
+memberRouter.post("/email", async function (req, res) {
+  console.log(req.body);
+  const result = await emailMember(req.body);
   res.json({ success: true, payload: result });
 });
 
