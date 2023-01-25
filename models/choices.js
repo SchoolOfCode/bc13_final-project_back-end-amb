ac2ff3234cad62ebaf9cb6defef09b6b23c01de4
Celia Bocking <celiabocking99@gmail.com>
2023-01-25 14:09:40 +0000

    stuff

--- models/choices.js
+++ models/choices.js
@@ -11,5 +11,6 @@ export async function getChoices(trip_id) {
   const getChoices = await query(
     `SELECT trip.trip_id, trip.trip_name, choices.choice_id, choices.category, choices.choice_name FROM trip INNER JOIN poll ON trip.trip_id = poll.trip_id INNER JOIN choices ON choices.poll_id = poll.poll_id WHERE trip.trip_id = ${trip_id}`
   );
+  console.log(getChoices);
   return getChoices.rows;
 }
